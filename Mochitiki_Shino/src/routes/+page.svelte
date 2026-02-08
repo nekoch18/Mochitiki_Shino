228f0de5bd4d96488d0ea5eca5db77cfc8b9d2f4
nekoch18 <mpfmoflppm2275@gmail.com>
2026-02-08 21:02:49 +0900

    画像を差替え

--- Mochitiki_Shino/src/routes/+page.svelte
+++ Mochitiki_Shino/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    import Shino from '$lib/assets/6904231-1.webp';
+    import Shino from '$lib/assets/xmv0rlxmv0r.webp';
 </script>
-<h1 class="text-3xl tracking-[0.2em] font-bold mb-12">Mochitiki Shino.</h1>
+<h1 class="text-3xl tracking-[0.2em] font-bold mb-18">Mochitiki Shino.</h1>
 <div class="relative w-screen left-1/2 -translate-x-1/2 overflow-visible">
     <svg class="w-screen max-w-none" viewBox="0 0 800 120">
         <path d="M0 60 H150 L170 40 L190 80 L210 20 L230 60 H370 L390 40 L410 80 L430 20 L450 60 H800" stroke="black" stroke-width="4" fill="none" />
